2c7db1c5d990940133deef6ed1cc2df6d2b7e951
ID-JaRa <matvejvolkov242@gmail.com>
2024-07-26 20:30:42 +0300

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,5 @@
+completed_tasks = "12"
+hours_spent = "1.5"
+name_course = "Python"
+time_for_one_leeson = (12 // 1.5)
+print (name_course,(', всего задач:'),completed_tasks, (', затрачено часов:'), hours_spent,(',среднее время выполнения:'), time_for_one_leeson)
